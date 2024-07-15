e21dd91050fc786a2f1b2fddebf4de963b2922ea
sum bùi <sumbn37k02.1@gmail.com>
2024-07-15 23:55:54 +0700

    update maints

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,14 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  // app.enableCors();
-  app.enableCors({
-    origin: ['https://YOUR-APP-NAME.vercel.app', 'http://localhost:3000'],
-    methods: 'GET, HEAD, PUT, POST, DELETE, OPTIONS, PATCH',
-    credentials: true,
-    allowedHeaders:
-      'Origin, X-Requested-With, Content-Type, Accept, Authentication, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma',
-  });
+  app.enableCors();
+  // app.enableCors({
+  //   origin: ['https://YOUR-APP-NAME.vercel.app', 'http://localhost:3000'],
+  //   methods: 'GET, HEAD, PUT, POST, DELETE, OPTIONS, PATCH',
+  //   credentials: true,
+  //   allowedHeaders:
+  //     'Origin, X-Requested-With, Content-Type, Accept, Authentication, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma',
+  // });
   await app.listen(3000);
 }
 bootstrap();
