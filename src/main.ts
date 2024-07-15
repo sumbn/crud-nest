a566b52703ce2d56d152d9c7874da814fd627572
sum bùi <sumbn37k02.1@gmail.com>
2024-07-16 00:21:47 +0700

    create dist

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
+  // app.enableCors();
+  app.enableCors({
+    origin: process.env.FRONTEND_URL, // Add your frontend URL here
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
+  });
   await app.listen(3000);
 }
 bootstrap();
