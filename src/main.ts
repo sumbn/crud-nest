ebe6176c5090c84f14cfa62f026e87155edbfdb4
sum bùi <sumbn37k02.1@gmail.com>
2024-07-16 00:09:46 +0700

    changed

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  // app.enableCors({
-  //   origin: ['https://YOUR-APP-NAME.vercel.app', 'http://localhost:3000'],
-  //   methods: 'GET, HEAD, PUT, POST, DELETE, OPTIONS, PATCH',
-  //   credentials: true,
-  //   allowedHeaders:
-  //     'Origin, X-Requested-With, Content-Type, Accept, Authentication, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma',
-  // });
   await app.listen(3000);
 }
 bootstrap();
